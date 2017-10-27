62a3be9c87bc07382f3ed8bacc787d64797b603b
hawkli <hawkli@yespowering.com>
2017-10-27 19:48:27 +0800

    进行到房屋信息爬取部分

--- house_info.py
+++ house_info.py
@@ -2,5 +2,13 @@ import sys
 import re
 import csv
 import requests
+from bs4 import BeautifulSoup
 
-def 
+page_num = ['pg', 1]
+
+def get_bsobj(url):
+    page = requests.get(url)# .text
+
+    if page.status_code == 200:
+        html = page.text
+        bs4obj = BeautifulSoup(html)
